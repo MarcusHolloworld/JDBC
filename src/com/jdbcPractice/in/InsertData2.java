d622601c89c5c364e155a359069ebd4d3cbcdf31
Rohit Bhowmik <rohitbhowmik19@gmail.com>
2024-01-26 12:59:17 +0530

    modified

--- src/com/jdbcPractice/in/InsertData2.java
+++ src/com/jdbcPractice/in/InsertData2.java
@@ -1,38 +1,40 @@
 package com.jdbcPractice.in;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class InsertData2 {
 	public static void main(String[] args) {
 		try {
+
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			String url = "jdbc:mysql://localhost:3306/jdbc";
 			String userName = "root";
 			String password = "rohit";
+
 			Connection con = DriverManager.getConnection(url,userName,password);
 
-			String q = "insert into table1 (tName,tCity) values (?,?);";
+			String q = "insert into table1(tName,tCity) values(?,?)";
 
 			PreparedStatement pstmt = con.prepareStatement(q);
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+			Scanner scanner = new Scanner(System.in);
 			System.out.print("Enter name: ");
-			String name = br.readLine();
+			String name = scanner.nextLine();
 
 			System.out.print("Enter city: ");
-			String city = br.readLine();
+			String city = scanner.nextLine();
 
 			pstmt.setString(1,name);
 			pstmt.setString(2,city);
-			pstmt.executeUpdate();
 
-			System.out.println("Inserted..");
+			pstmt.executeUpdate();
+			System.out.println("Inserted...");
 			con.close();
 
 		} catch (Exception ex) {
